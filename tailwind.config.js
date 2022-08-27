cbb200eac574eff9ededeb2a8692b0af4c42259b
Nickothan <s1nicolasrg@gmail.com>
2022-08-27 12:06:27 -0500

    Section 1 from home desktop.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/assets/images/image1.png')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+      colors: {
+        "gris-trasparent": "rgba(255, 255, 255, 0.5)",
+      },
+    },
   },
   plugins: [],
 };
